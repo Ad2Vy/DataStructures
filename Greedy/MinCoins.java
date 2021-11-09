c0b0f3a031b5dcd3af48c1589309dfd82cc884a3
Ad2Vy <mailtoaditya02@gmail.com>
2021-11-09 19:09:31 +0530

    initial Greedy commit

--- Greedy/MinCoins.java
+++ Greedy/MinCoins.java
@@ -1,4 +1,25 @@
 package Greedy;
 
 public class MinCoins {
+    public static int minCoin(int N){
+        int count=0;
+        int[] arr={10,5,2,1};
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<=N){
+                int coin= N/arr[i];
+                N=N- arr[i]*coin;
+                System.out.println("coins of "+arr[i]+" are : "+coin);
+                count+=coin;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int n=52;
+        System.out.println("min no. of coins for required sum of money is: "+minCoin(n));
+    }
+
+
 }
