be35c11f03e5fac542998f29899c67f4c6ff3731
Ad2Vy <mailtoaditya02@gmail.com>
2021-11-09 20:15:30 +0530

    Added ActivitySelection

--- Greedy/ActivitySelection.java
+++ Greedy/ActivitySelection.java
@@ -1,2 +1,58 @@
-package Greedy;public class ActivitySelection {
+package Greedy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ActivitySelection {
+    public static int maxActivity(pair[] arr){
+        int count=0;
+        Arrays.sort(arr,new MyCmp());
+        for(int i=1;i<arr.length;i++){
+            if(arr[i].getStartTime()<arr[i-1].getEndTime()){
+                count++;
+            }
+        }
+
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        pair[] arr={
+        (new pair(1,3)),
+        (new pair(2,4)),
+        (new pair(5,8)),
+        (new pair(6,10))};
+
+        System.out.println("max no. of activity: "+maxActivity(arr));
+
+    }
+
+    //implements comparator interface (used ot order user defined class objects)
+    static class MyCmp implements Comparator<pair>
+{
+        public int compare(pair a1, pair a2)
+        {
+            return a1.getEndTime() - a2.getEndTime();
+        }
+}
+
+
+}
+class pair{
+    int startTime;
+    int endTime;
+    pair(int s,int e){
+        this.startTime=s;
+        this.endTime=e;
+    }
+    int getStartTime(){
+        return startTime;
+    }
+    int getEndTime(){
+        return endTime;
+    }
+
 }
