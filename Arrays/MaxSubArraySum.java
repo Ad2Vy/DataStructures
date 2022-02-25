7305b2a30e2f2acb45bb585e3a3e6c2dd8ad0b8d
Ad2Vy <mailtoaditya02@gmail.com>
2022-02-25 20:38:26 +0530

    added MaxSubArraySum

---
+++ Arrays/MaxSubArraySum.java
@@ -0,0 +1,53 @@
+package Arrays;
+
+public class MaxSubArraySum {
+    static int maxSum(int[] arr){
+        int max=arr[0],curMax=arr[0];
+        for(int i=1;i<arr.length;i++){
+            curMax=Math.max(curMax+arr[i],arr[i]);
+            max=Math.max(curMax,max);
+        }
+        return max;
+    }
+
+    static int longest(int[] arr){
+        //Brute force
+//        int max=0,count=1;
+//        for(int i=0;i<arr.length;i++){
+//            int freq=1;
+//            for(int j=i+1;j<arr.length;j++){
+//                if((arr[j]%2==0&&arr[j-1]%2!=0)  || (arr[j]%2!=0&&arr[j-1]%2==0)){
+//                    freq++;
+//                }
+//                else
+//                    break;
+//            }
+//            max=Math.max(max,freq);
+//        }
+//        return max;
+
+        //Efficient solution
+
+        int max=1,count=1;
+        for(int i=1;i<arr.length;i++){
+            if((arr[i]%2==0&&arr[i-1]%2!=0)  || (arr[i]%2!=0&&arr[i-1]%2==0)){
+                count++;
+                max=Math.max(max,count);
+            }
+            else
+                count=1;
+
+
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+//        int[] arr={2,3,-8,7,-1,2,3};
+//        System.out.println(maxSum(arr));
+
+        int[] arr={0,1,2,3,3,4,5,5,6,7,8};
+        System.out.println(longest(arr));
+
+    }
+}
